749436e8d8ea06975cf3763d36a1187b405a6b0f
Sanskar Nagdive <118511784+SanskarNagdive@users.noreply.github.com>
2026-02-08 22:21:10 +0530

    Update generate-review.js

--- netlify/functions/generate-review.js
+++ netlify/functions/generate-review.js
@@ -4,7 +4,7 @@ exports.handler = async (event) => {
 
 
     const response = await fetch(
-  `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${process.env.GEMINI_API_KEY}`,
+  `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
   {
     method: "POST",
     headers: {
@@ -23,6 +23,7 @@ exports.handler = async (event) => {
     })
   }
 );
+
 
 
 
